147b1fd11d426e010b167520fd6c6e50d8882d93
Denver Fernandes <sub367@gmail.com>
2016-04-14 22:15:27 +0100

    Add support for a player's walls.

--- src/aston/group12/model/Player.java
+++ src/aston/group12/model/Player.java
@@ -12,6 +12,7 @@ public abstract class Player {
 	private String playerID;
 	private String name;
 	private Pawn pawn;
+	private int walls;
 	
 	/**
 	 * Creates a new Player by initialising its name and pawn
@@ -22,7 +23,7 @@ public abstract class Player {
 	public Player(String name, Pawn pawn){
 		//Generate UUID
 		playerID = UUID.randomUUID().toString();
-		
+		walls = GameSession.STARTING_WALLS_NUM;
 		//Initialise values.
 		this.name = name;
 		this.pawn = pawn;
@@ -50,5 +51,23 @@ public abstract class Player {
 	 */
 	public Pawn getPawn() {
 		return pawn;
+	}
+	
+	/**
+	 * Gets the amount of walls that the player has.
+	 * @return the number of walls
+	 */
+	public int getWalls() {
+		return walls;
+	}
+	
+	/**
+	 * Decrements the number of walls that the player has by one.
+	 */
+	public void decrementWalls() {
+		if(walls == 0) {
+			throw new IllegalStateException("The number of walls cannot be decremented below 0");
+		}
+		walls--;
 	}
 }
