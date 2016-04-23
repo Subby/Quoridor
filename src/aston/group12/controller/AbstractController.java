87f38e77ecffad4314df4eb64b60a1b2cbe4f838
Denver Fernandes <sub367@gmail.com>
2016-04-23 21:45:27 +0100

    Add AbstractController for views

---
+++ src/aston/group12/controller/AbstractController.java
@@ -0,0 +1,30 @@
+package aston.group12.controller;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public abstract class AbstractController {
+	
+	/**
+	 * Loads a new screen and assigns it to the current {@link Stage}.
+	 * @param primaryStage the stage
+	 * @param layoutFile the name of the layout file
+	 */
+	protected void loadScreen(Stage primaryStage, String layoutFile){
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("../view/layouts/" + layoutFile));
+			Scene scene = new Scene(root);
+			primaryStage.setTitle("Quoridor");
+			primaryStage.getIcons().add(new Image("res/icons/favicon.png"));
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
